942a98ef827e5180c47d6c6caad24cc57ef90460
Stef <stef.jones@condenast.com>
2020-07-10 18:33:40 +0100

    fix: remove debug-run

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ const getPalette = async (pathToImage) => {
   const vibrantImage = new Vibrant(pathToImage);
   const colorPalette = await vibrantImage.getPalette();
 
-  if (process.env.NODE_ENV === "debug-run") {
-    console.debug(colorPalette);
-    console.info("success");
-  }
-
   return colorPalette;
 };
 
@@ -33,12 +28,6 @@ const getSerializablePalette = async (pathToImage) => {
 
   return serializablePalette;
 };
-
-if (process.env.NODE_ENV === "debug-run") {
-  getPalette("sample-images/vogue-uk_5d544f04d9c06a0008db33f0_1024.jpg").catch(
-    console.error
-  );
-}
 
 module.exports = {
   getPalette,
