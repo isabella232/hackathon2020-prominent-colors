673e3b6b9ad7feaf2f09af185f3bc7d1d7c5007c
Stef <stef.jones@condenast.com>
2020-07-10 12:43:59 +0100

    feat: add getPalette

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-const vibrant = require("node-vibrant");
+const Vibrant = require("node-vibrant");
 
-// TODO
+const getPalette = async (pathToImage) => {
+  const vibrantImage = new Vibrant(pathToImage);
+  const colorPalette = await vibrantImage.getPalette();
+
+  if (process.env.NODE_ENV === "debug-run") {
+    console.debug(colorPalette);
+    console.info("success");
+  }
+
+  return colorPalette;
+};
+
+if (process.env.NODE_ENV === "debug-run") {
+  getPalette("sample-images/vogue-uk_5d544f04d9c06a0008db33f0_1024.jpg").catch(
+    console.error
+  );
+}
+
+module.exports = {
+  getPalette,
+};
